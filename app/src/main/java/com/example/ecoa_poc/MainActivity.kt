de3a8a3022cc8d6536d0d8454a8ab7d293da96b0
Himanshu Yadav1 <t_himanshu.y@tatadigital.com>
2023-10-17 11:37:06 +0530

    Added click event and color changes

--- app/src/main/java/com/example/ecoa_poc/MainActivity.kt
+++ app/src/main/java/com/example/ecoa_poc/MainActivity.kt
@@ -11,14 +11,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var previousSVGPath:SVGPath? = null
 
 
         val androidRichPathView = findViewById<SVGPathView>(R.id.view)
+        var previousColor:Int =0
         androidRichPathView.onPathClickListener = object : SVGPath.OnPathClickListener {
             override fun onClick(svgPath: SVGPath) {
-                svgPath.fillColor= Color.BLUE
-                getMapList().get(svgPath.name)?.let {
-                    setAlert(it.toString())
+                if (svgPath.name !=null){
+                    if (previousSVGPath!=null){
+                        val storedObj =previousSVGPath!!.name?.let {
+                            androidRichPathView.findRichPathByName(it)
+                        }
+                        storedObj?.fillColor = previousColor
+                        previousSVGPath = svgPath
+                        previousColor = svgPath.fillColor
+                        svgPath.fillColor = Color.BLUE
+                    }else{
+                        previousColor = svgPath.fillColor
+                        svgPath.fillColor= Color.BLUE
+                        previousSVGPath = svgPath
+                    }
+
+                    getMapList().get(svgPath.name)?.let {
+                        setAlert(it.toString())
+                    }
                 }
             }
         }
@@ -26,11 +43,40 @@ class MainActivity : AppCompatActivity() {
 
     fun getMapList():HashMap<String, String>{
         val theMap = HashMap<String, String>()
-        theMap["leftEye"] = "The eye is made up of three coats, which enclose the optically clear aqueous humour, lens, and vitreous body. The outermost coat consists of the cornea and the sclera; the middle coat contains the main blood supply to the eye and consists, from the back forward, of the choroid, the ciliary body, and the iris."
-        theMap["leftHand"] = "Examining data on about 400,000 people, scientists discovered that the left and right hemispheres of the brain were better connected and more coordinated in regions involving language in left-handed people. These traits suggest that left-handed individuals may have superior verbal skills."
-        theMap["rightHand"] = "Examining data on about 400,000 people, scientists discovered that the left and right hemispheres of the brain were better connected and more coordinated in regions involving language in left-handed people. These traits suggest that left-handed individuals may have superior verbal skills."
-        theMap["rightLowerHand"] = "Examining data on about 400,000 people, scientists discovered that the left and right hemispheres of the brain were better connected and more coordinated in regions involving language in left-handed people. These traits suggest that left-handed individuals may have superior verbal skills."
-        theMap["rightPalms"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backHead"] = "The part of the maxilla in which the 4 upper incisors develop, which forms the primary palate, and underlies the philtrum and upper lip."
+        theMap["leftHeap"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftSolder"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftBack"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backLeftThigh"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backLeftLeg"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backRightLeg"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftBackHand"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftHand"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftLittleFinger"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftRingFinger"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftMidleFinger"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftIndexFinger"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["leftThumb"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backRightHip"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backRightBack"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["backRightHand"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+
+
+        theMap["frontHead"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftChest"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["rightChest"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftSolder"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftBishapes"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftHand"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftHand"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftKnee"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftLeg"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftLeg"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftLeg"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["frontLeftPalm"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["rightSolder"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["rightPlam"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
+        theMap["face"] = "According to astrology, a man's right itchy palm indicates good days. Right wrist or palm itching predicts the possibility of getting unexpected financial assistance. The person might get delayed payment. They could profit from a lot of things, win the lottery, or by making getting gains in the stock market"
 
         return theMap
     }
